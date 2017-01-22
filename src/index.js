8849664852f356948c6cd670faa8e3815fb12120
Thomas Breier <breierthomas@gmail.com>
2017-01-22 09:20:23 -0800

    HUHHH

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function queryNextbus(all, callback) {
       //http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=unitrans&r=O&s=22175
         host: 'webservices.nextbus.com',
         //path: '/service/publicXMLFeed?command=predictions&a=unitrans&r=O&s=22175',
-        path: '/service/publicXMLFeed?command=predictions&a=unitrans&stopId=188',
+        path: '/service/publicXMLFeed?command=predictions&a=unitrans&stopId=175',
         method: 'GET'
     };
 
