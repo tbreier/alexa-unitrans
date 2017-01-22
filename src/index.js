dec3c604dc6a82478d574f5b8b2597a562d88841
Calvin Huang <clvnln794@gmail.com>
2017-01-22 00:21:31 -0800

    Initial Build
    Version 0.9 of an Alexa Skill for Unitrans

---
+++ src/index.js
@@ -0,0 +1,139 @@
+'use strict';
+
+var Alexa = require('alexa-sdk');
+var APP_ID = undefined; // TODO replace with your app ID (OPTIONAL).
+var recipes = require('./recipes');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    // To enable string internationalization (i18n) features, set a resources object.
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    //Use LaunchRequest, instead of NewSession if you want to use the one-shot model
+    // Alexa, ask [my-skill-invocation-name] to (do something)...
+    'LaunchRequest': function () {
+        this.attributes['speechOutput'] = this.t("WELCOME_MESSAGE", this.t("SKILL_NAME"));
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+        this.attributes['repromptSpeech'] = this.t("WELCOME_REPROMT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+	'AllIntent': function () {
+		
+		this.emit(':tell', "All Lines");
+	},
+	
+    'RecipeIntent': function () {
+        var itemSlot = this.event.request.intent.slots.Item;
+        var itemName; //
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value;
+			}
+			
+		var result = {'A': 10, 'B': 15};
+		
+		
+
+        //var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
+        //var recipes = this.t("RECIPES");
+        var recipe = result[itemName];
+		
+		
+
+        if (recipe) {
+            this.attributes['speechOutput'] = recipe; //says the recipe
+            //this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
+            this.emit(':tell',"Your bus will be arriving in" + recipe + "minutes");
+        } else {
+            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
+            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
+            if (itemName) {
+                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
+            } else {
+                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes['speechOutput'] = speechOutput;
+            this.attributes['repromptSpeech'] = repromptSpeech;
+
+            this.emit(':ask', speechOutput, repromptSpeech);
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
+        this.attributes['repromptSpeech'] = this.t("HELP_REPROMT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'SessionEndedRequest':function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    }
+};
+
+var languageStrings = {
+    "en-GB": {
+        "translation": {
+            "RECIPES": recipes.RECIPE_EN_GB,
+            "SKILL_NAME": "myBus",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, when\'s the A bus coming? ... Now, what can I help you with.",
+            "WELCOME_REPROMT": "For instructions on what you can say, please say help me.",
+            "DISPLAY_CARD_TITLE": "%s  - Route %s.",
+            "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMT": "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
+            "STOP_MESSAGE": "Goodbye!",
+            "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
+            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s. ",
+            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
+            "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
+        }
+    },
+    "en-US": {
+        "translation": {
+            "RECIPES" : recipes.RECIPE_EN_US,
+            "SKILL_NAME" : "myBus",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, when\'s the A bus coming? ... Now, what can I help you with.",
+            "WELCOME_REPROMT": "For instructions on what you can say, please say help me.",
+            "DISPLAY_CARD_TITLE": "%s  - Route for %s.",
+            "HELP_MESSAGE": "You can ask questions such as, when\'s the A bus, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMT": "You can say things like, when\'s the A bus, or you can say exit...Now, what can I help you with?",
+            "STOP_MESSAGE": "Goodbye!",
+            "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
+            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the route for %s. ",
+            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that route. ",
+            "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
+        }
+    },
+    "de-DE": {
+        "translation": {
+            "RECIPES" : recipes.RECIPE_DE_DE,
+            "SKILL_NAME" : "Assistent für Minecraft in Deutsch",
+            "WELCOME_MESSAGE": "Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?",
+            "WELCOME_REPROMT": "Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.",
+            "DISPLAY_CARD_TITLE": "%s - Rezept für %s.",
+            "HELP_MESSAGE": "Du kannst beispielsweise Fragen stellen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?",
+            "HELP_REPROMT": "Du kannst beispielsweise Sachen sagen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?",
+            "STOP_MESSAGE": "Auf Wiedersehen!",
+            "RECIPE_REPEAT_MESSAGE": "Sage einfach „Wiederholen“.",
+            "RECIPE_NOT_FOUND_MESSAGE": "Tut mir leid, ich kenne derzeit ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "das Rezept für %s nicht. ",
+            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "dieses Rezept nicht. ",
+            "RECIPE_NOT_FOUND_REPROMPT": "Womit kann ich dir sonst helfen?"
+        }
+    }
+};
