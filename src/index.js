30f39edac4b806aeb13cfda0c495381e0931c508
Thomas Breier <breierthomas@gmail.com>
2017-01-22 01:50:30 -0800

    text

--- src/index.js
+++ src/index.js
@@ -206,15 +206,15 @@ var languageStrings = {
         "translation": {
             "RECIPES" : recipes.RECIPE_EN_US,
             "SKILL_NAME" : "myBus",
-            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, when\'s the A bus coming? ... Now, what can I help you with.",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, when\'s the A bus?, or, all arrivals. ... Now, what can I help you with.",
             "WELCOME_REPROMT": "For instructions on what you can say, please say help me.",
             "DISPLAY_CARD_TITLE": "%s  - Route for %s.",
-            "HELP_MESSAGE": "You can ask questions such as, when\'s the A bus, or, you can say exit...Now, what can I help you with?",
-            "HELP_REPROMT": "You can say things like, when\'s the A bus, or you can say exit...Now, what can I help you with?",
+            "HELP_MESSAGE": "You can ask questions such as, when\'s the A bus, all arrivals, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMT": "You can say things like, when\'s the A bus, all arrivals, or you can say exit...Now, what can I help you with?",
             "STOP_MESSAGE": "Goodbye!",
             "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
-            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
-            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the route for %s. ",
+            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, there don't seem to be any arrivals for ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the %s route. ",
             "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that route. ",
             "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
         }
